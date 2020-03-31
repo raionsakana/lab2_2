95610bb9dd9175e816329c69b7aa1fd270dc1a58
Patryk Janowski <215745@edu.p.lodz.pl>
2020-03-31 20:40:53 +0200

    BeforeEach for tests added

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
@@ -1,11 +1,20 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcherMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderBehaviourTest {
 
+    private SimilarityFinder similarityFinder;
+
+    @BeforeEach
+    public void init() {
+        similarityFinder = new SimilarityFinder(new SequenceSearcherMock());
+    }
+    
     @Test
     void stateTest() {
 
--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -1,12 +1,23 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcherMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderStateTest {
 
+    private SimilarityFinder similarityFinder;
+
+    @BeforeEach
+    public void init() {
+        similarityFinder = new SimilarityFinder(new SequenceSearcherMock());
+    }
+
     @Test
     void stateTest() {
+
+
     }
 }
