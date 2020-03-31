4ce952925db28fb4edda632477953b49028266a0
Patryk Janowski <215745@edu.p.lodz.pl>
2020-03-31 19:54:10 +0200

    testing classes created

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimilarityFinderBehaviourTest {
+
+    @Test
+    void stateTest() {
+    }
+}
---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimilarityFinderStateTest {
+
+    @Test
+    void stateTest() {
+    }
+}
